095b0ce076b480c01153226025153a5d4a02fb9f
youhyeon <youhyeon@wishket.com>
2017-12-11 13:56:19 +0900

    setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,22 @@ setup(
     version='0.1.0',
     packages=['drf_swagger'],
     url='https://github.com/koyouhun/drf_swagger',
+    download_url='https://github.com/koyouhun/drf_swagger/archive/0.1.0.tar.gz',
     license='BSD',
     author='YouHyeon Ko',
     author_email='koyouhun@gmail.com',
     description='Django REST Framework + Swagger',
     keywords=['django', 'swagger', 'api', 'documentation'],
+    classifiers=[
+          'Development Status :: 1',
+          'License :: BSD',
+          'Programming Language :: Python :: 2.7'
+      ],
+    install_requires=[
+        'Django>=1.7',
+        'PyYAML>=3.12',
+        'six>=1.10.0',
+        'uritemplate>=3.0.0',
+        'djangorestframework>=3.6.0'
+    ]
 )
